bbd4f642792b48fb005dd2f5c2ebf18ddda372d7
MrBleuGlacial <bleuglacial@gmail.com>
2015-03-23 17:32:00 +0100

    Script Clean-up
    Début de script pour permettre une visualisation plus pertinente.
    Correction d'autres détails mineurs.

---
+++ scriptTulip_cleanup.py
@@ -0,0 +1,98 @@
+# Powered by Python 2.7
+
+# To cancel the modifications performed by the script
+# on the current graph, click on the undo button.
+
+# Some useful keyboards shortcuts : 
+#   * Ctrl + D : comment selected lines.
+#   * Ctrl + Shift + D  : uncomment selected lines.
+#   * Ctrl + I : indent selected lines.
+#   * Ctrl + Shift + I  : unindent selected lines.
+#   * Ctrl + Return  : run script.
+#   * Ctrl + F  : find selected text.
+#   * Ctrl + R  : replace selected text.
+#   * Ctrl + Space  : show auto-completion dialog.
+
+from tulip import *
+
+# the updateVisualization(centerViews = True) function can be called
+# during script execution to update the opened views
+
+# the pauseScript() function can be called to pause the script execution.
+# To resume the script execution, you will have to click on the "Run script " button.
+
+# the runGraphScript(scriptFile, graph) function can be called to launch another edited script on a tlp.Graph object.
+# The scriptFile parameter defines the script name to call (in the form [a-zA-Z0-9_]+.py)
+
+# the main(graph) function must be defined 
+# to run the script on the current graph
+
+def main(graph): 
+	Acces = graph.getStringProperty("Acces")
+	CreationXML = graph.getStringProperty("CreationXML")
+	Genealogie = graph.getStringProperty("Genealogie")
+	Hierarchie = graph.getStringProperty("Hierarchie")
+	ID_Fiche = graph.getStringProperty("ID_Fiche")
+	Langage = graph.getStringProperty("Langage")
+	OrganisationName = graph.getStringVectorProperty("OrganisationName")
+	OrganisationRole = graph.getStringVectorProperty("OrganisationRole")
+	Tags = graph.getStringProperty("Tags")
+	Titre = graph.getStringProperty("Titre")
+	viewBorderColor = graph.getColorProperty("viewBorderColor")
+	viewBorderWidth = graph.getDoubleProperty("viewBorderWidth")
+	viewColor = graph.getColorProperty("viewColor")
+	viewFont = graph.getStringProperty("viewFont")
+	viewFontSize = graph.getIntegerProperty("viewFontSize")
+	viewLabel = graph.getStringProperty("viewLabel")
+	viewLabelBorderColor = graph.getColorProperty("viewLabelBorderColor")
+	viewLabelBorderWidth = graph.getDoubleProperty("viewLabelBorderWidth")
+	viewLabelColor = graph.getColorProperty("viewLabelColor")
+	viewLabelPosition = graph.getIntegerProperty("viewLabelPosition")
+	viewLayout = graph.getLayoutProperty("viewLayout")
+	viewMetric = graph.getDoubleProperty("viewMetric")
+	viewRotation = graph.getDoubleProperty("viewRotation")
+	viewSelection = graph.getBooleanProperty("viewSelection")
+	viewShape = graph.getIntegerProperty("viewShape")
+	viewSize = graph.getSizeProperty("viewSize")
+	viewSrcAnchorShape = graph.getIntegerProperty("viewSrcAnchorShape")
+	viewSrcAnchorSize = graph.getSizeProperty("viewSrcAnchorSize")
+	viewTexture = graph.getStringProperty("viewTexture")
+	viewTgtAnchorShape = graph.getIntegerProperty("viewTgtAnchorShape")
+	viewTgtAnchorSize = graph.getSizeProperty("viewTgtAnchorSize")
+	
+	Degree = graph.getDoubleProperty("Degree")
+
+#  TODO :
+#	dataSet = tlp.getDefaultPluginParameters("Degree",graph)
+#	dataSet["Property"] = Degree	
+#	graph.applyAlgorithm("Degree",dataSet)
+#  ERROR : plugin "Degree" doesn't exist 
+
+################### WARNING ###################
+# Appel du plug-in Degree non finalise. 
+# Necessite de remplir la propriete Degree a la main en usant le plugin Degree via la GUI
+# pour la suite du traitement. 
+# 1) Utiliser Algorithm/Graph/Degree
+# 2) Copier viewMetric dans Degree via la Spreadsheet view
+	
+	for n in graph.getNodes():
+		if(ID_Fiche[n] == ""):
+			if(Degree[n] <= 3):
+				graph.delNode(n)
+	
+	
+	print "--- Ended ---"
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
